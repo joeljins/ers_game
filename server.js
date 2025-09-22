15ac67835c20648fbc31e67f4cc13c91c8f1e1f1
jj3249 <jj3249@drexel.edu>
2025-09-22 01:20:52 -0400

    initial

---
+++ server.js
@@ -0,0 +1,23 @@
+async function getDeck(){
+    const url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+    try{
+        const response = await fetch(url);
+        if (!response.ok){
+            throw new Error()
+        }
+    const result = await response.json();
+    console.log(result);
+    } catch(error) {
+        console.log(error.message)
+    }
+}
+
+async function setDeck(id){
+    const url = 'https://www.deckofcardsapi.com/api/deck/{$id}/draw/?count=1';
+    try{
+        const response = await fetch(url)
+    }
+}
+function deck(){
+
+}
